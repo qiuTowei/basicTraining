2e19bdcb066f07da447a43ac339c63c4dc4ad952
qiuwei <qiuwei1991@yeah.net>
2023-10-26 13:17:46 +0800

    init project

---
+++ src/main/java/com/example/basictraining/BasicTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.basictraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicTrainingApplication.class, args);
+    }
+
+}
